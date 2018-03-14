fb2b264526dce324a45e82600b15786be9c01224
amri <a.amri.91@gmail.com>
2018-03-14 17:56:20 +0100

    Fix for single docker

--- scripts/singleDocker.sh
+++ scripts/singleDocker.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-echo stopping current container
-docker stop $APPNAME-container
+echo removing the old image
+docker rmi notphilips/"$APPNAME":latest -f
 
-echo removing current container
-docker rm $APPNAME-container
+echo pulling a new image
+docker pull notphilips/"$APPNAME":latest
 
-echo running new container
-docker run --name=$APPNAME-container --restart=always -p $PORT:80 -d notphilips/$APPNAME:latest
+echo stopping the current container
+docker stop "$APPNAME"-container
+
+echo removing the current container
+docker rm "$APPNAME"-container
+
+echo running a new container
+docker run --name="$APPNAME"-container --restart=always -p $PORT:80 -d notphilips/"$APPNAME":latest
 
 echo done
