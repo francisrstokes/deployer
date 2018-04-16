627dab2fec1ca319bb36f8a9b33c34bbaccd6732
amri <a.amri.91@gmail.com>
2018-04-16 13:08:42 +0200

    update config

--- config/environments/defaults.js
+++ config/environments/defaults.js
@@ -8,5 +8,8 @@ module.exports = {
   },
   logger: bunyan.createLogger({name: `Deployer`}),
   DOCKER_USERNAME: process.env.DOCKER_USERNAME,
-  DOCKER_PASSWORD: process.env.DOCKER_PASSWORD
+  DOCKER_PASSWORD: process.env.DOCKER_PASSWORD,
+  AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID
 };
